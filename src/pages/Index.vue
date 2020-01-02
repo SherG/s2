fe0450341222a8156f7899f1d9d08a0b2e3014f2
sherrie gossett <sherrie.g5@gmail.com>
2020-01-01 23:50:04 -0500

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -95,7 +95,7 @@
 
           
           <div class="text-lg sm:text-lg mb-16">
-            <form method="POST" action="#" name="contact" class="mb-12" data-netlify="true">
+            <form method="POST" action="#" name="contact2" class="mb-12" data-netlify="true">
           
               <div class="flex flex-wrap mb-6 -mx-4">
                   <div class="w-full md:w-1/2 mb-6 md:mb-0 px-4">
