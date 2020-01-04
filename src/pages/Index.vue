d92822c007338f4489b39cb7573a81865adb8a7a
sherrie gossett <sherrie.g5@gmail.com>
2020-01-04 00:41:54 -0500

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -11,9 +11,9 @@
     </div> <!-- end hero -->
 
   <div class="container-inner mx-auto">
-      <p class="text-lg sm:text-xl"> Digital marketing — with its thousands of tools and tactics — can be disorienting and lead to fragmented efforts. SignalFox is a marketing advisory service that helps small business owners make sense of marketing in order to act effectively. </p><br/>
+      <p class="text-lg sm:text-xl"> Digital marketing — with its thousands of tools and tactics — can be disorienting and lead to fragmented efforts. SignalFox helps small business owners make sense of marketing in order to act effectively. </p><br/>
 
-<p class="text-lg sm:text-xl">This site grew out of conversations I had with small business owners who were looking for a way to make better decisions and avoid wasted marketing spend.
+<p class="text-lg sm:text-xl">This service grew out of conversations I had with small business owners who were looking for a way to make better decisions and avoid wasted marketing spend.
  </p>
 
       <div class="flex justify-between items-center py-6">
